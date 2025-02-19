a510842b14099e9a450b5400cf5a48f08f49c865
tritoncg <admin@tritoncg.com>
2025-02-19 01:33:34 +0000

    app created with sign in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,43 @@
+import Login from "@/views/Login";
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="flex justify-center items-center min-h-screen">
+      <div className="w-full max-w-xs p-6">
+        <h2
+          className="text-center"
+          style={{ fontSize: "clamp(2.125rem, 1.6316rem + 2.6316vw, 4rem)" }}
+        >
+          Sign In
+        </h2>
+        <form action="" className="flex flex-col gap-4">
+          <input
+            type="email"
+            className="bg-[#224957] rounded-xl text-white py-2 px-3 outline-none"
+            placeholder="Email"
+          />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+          <input
+            type="password"
+            className="bg-[#224957] rounded-xl text-white py-2 px-3 outline-none"
+            placeholder="password"
+          />
+
+          <span className="flex justify-center items-center gap-2">
+            <input
+              type="checkbox"
+              id="remember"
+              className="w-5 h-5 appearance-none border border-[#224957] rounded-md bg-[#224957] checked:border-transparent checked:before:content-['✔'] checked:before:text-white checked:before:flex checked:before:items-center checked:before:justify-center checked:before:w-full checked:before:h-full"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <label htmlFor="remember" className="ml-2 ">
+              Remember me
+            </label>
+          </span>
+          <button className="bg-[#2BD17E] rounded-md p-2">Login</button>
+        </form>
+      </div>
+    </main>
   );
 }
---
+++ src/views/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <main>
+        <div>
+              <h2 className='' style={{fontSize: 'clamp(1.5rem, 0.8421rem + 3.5088vw, 4rem)'}}>Login</h2>
+                <form action="">
+                        <input type="email" className='bg-transparent' />
+                </form> 
+        </div>
+    </main>
+  )
+}
+
+export default Login
